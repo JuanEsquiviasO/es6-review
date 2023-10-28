f91b8a29d2aa1de7fed6ccacdf4d646fd2688a6b
Juan Esquivias <juanesquiviasotazu@gmail.com>
2023-10-28 16:03:45 -0500

    spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -10,3 +10,21 @@ let user = { username: 'Max', age: 24 };
 let { username, age } = user;
 console.log(username, age);
 console.log(username, user.age);
+
+// spread operator
+
+let person = { name: 'Roy', age: 28 };
+let country = 'CAN';
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+	console.log(values);
+	console.log(num + values[0]);
+	return num + values[0];
+}
+
+sum(1, 1, 2, 3);
