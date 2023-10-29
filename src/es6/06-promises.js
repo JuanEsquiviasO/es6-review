e8643ddf220d0ab1f69f7f05d6508514af626a86
Juan Esquivias <juanesquiviasotazu@gmail.com>
2023-10-29 14:31:55 -0500

    ES6 clases

--- src/es6/06-promises.js
+++ src/es6/06-promises.js
@@ -1,17 +1,3 @@
-// const anotherFunction = () => {
-// 	return new Promise((resolve, reject) => {
-// 		if (false) {
-// 			resolve('Hey!!');
-// 		} else {
-// 			reject('Whoops!');		}
-// 	})
-// }
-
-// anotherFunction()
-// 	.then(response => console.log(response))
-// 	.catch(err => console.log(err));
-
-
 const anotherFunction = () => {
 	return new Promise((resolve, reject) => {
 		if (true) {
