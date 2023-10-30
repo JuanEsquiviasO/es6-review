eac412024960954b790741f7d825a060d9503ba9
Juan Esquivias <juanesquiviasotazu@gmail.com>
2023-10-30 14:55:42 -0500

    ES7: exponentiation operator y array includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 3, 4, 6, 7, 8];
+
+console.log(numbers.includes(9));
+
+const list = ['Roy', 'Rick', 'Max', 'Ben', 'Miriya'];
+
+console.log(list.includes('roy'));
+console.log(list.includes('Exedore'));
+console.log(list.includes('Bretai'));
